75ed73332944e2032f1789569c9f8f6582c2a008
lukasz.szymankiewicz <lukasz.szymankiewicz@wmglobal.com>
2022-02-22 08:55:14 +0100

    Tile Pallete prototype added

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module ltools
 go 1.16
 
 require (
-	github.com/NYTimes/gziphandler v1.1.1 // indirect
-	github.com/hajimehoshi/ebiten/v2 v2.2.4
+	github.com/NYTimes/gziphandler v1.1.1
+	github.com/hajimehoshi/ebiten/v2 v2.2.5
 )
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"image"
+    "image"
+	"image/color"
 	_ "image/png"
 	"log"
+    "fmt"
 )
 
 const (
@@ -16,6 +18,9 @@ const (
 	tilesPerRow  = 6
 	tileWidth    = 32
 	tileHeight   = 32
+    palleteCols  = 6 
+    palleteRows  = 10 
+
 )
 
 type Game struct{}
@@ -35,13 +40,29 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "LEVEL EDITOR")
+func cursorInRect (cursor_x, cursor_y, rect_x1, rect_y1, rect_x2, rect_y2 int) bool {
+    if (cursor_x >= rect_x1 && cursor_x <= rect_x2) && (cursor_y >= rect_y1 && cursor_y <= rect_y2) {
+        return true
+    }
+    return false
+}
 
-	var currentTile int
-	var palleteCols int = 6 
+func drawCursorOnPallete(screen *ebiten.Image, x int, y int) {
+    ebitenutil.DrawRect(
+        screen,
+        float64((int(((x-palleteX)/tileWidth) * tileWidth) + palleteX)),
+        float64((int(((y-palleteY)/tileHeight) * tileHeight) + palleteY)),
+        tileWidth,
+        tileHeight,
+        color.White,
+    )
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+    var currentTile int
 
     tilesetWidth, tilesetHeight := img.Size()
+
 	var tilesheetRows  int = tilesetWidth / tileWidth
 	var tilesheetCols int = tilesetHeight  / tileWidth
 	var tilesNum int = tilesheetCols * tilesheetRows
@@ -57,14 +78,32 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		subImg := img.SubImage(tileRect).(*ebiten.Image)
 		op := &ebiten.DrawImageOptions{}
         op.GeoM.Translate(
-             float64((currentTile % palleteCols) * tileWidth),
-             float64((currentTile / palleteCols) * tileHeight),
+             float64(((currentTile % palleteCols) * tileWidth) + palleteX),
+             float64(((currentTile / palleteCols) * tileHeight) + palleteY),
         )
 		screen.DrawImage(subImg, op)
 
 		currentTile++
+        if currentTile >= palleteCols * palleteRows {
+            break
+        }
 	}
 
+    // mouse
+    x,y := ebiten.CursorPosition()
+    msg := fmt.Sprintf("x=%d, y=%d \n", x, y)
+	ebitenutil.DebugPrint(screen, msg)
+
+    if cursorInRect(
+        x,
+        y,
+        palleteX,
+        palleteY,
+        (palleteX + (tileWidth * palleteCols)),
+        (palleteY + (tileHeight * palleteRows)),
+    ) {
+        drawCursorOnPallete(screen, x, y)
+    }
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -74,6 +113,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 // placeholder gonna hold place
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
+
 	ebiten.SetWindowTitle("LTOOLS - LIGHTER DEVELOPMENT TOOLS")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
