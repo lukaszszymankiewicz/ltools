02a5e1301ea9294667b2b5b5bafae29408d4dfce
lukasz.szymankiewicz <lukasz.szymankiewicz@wmglobal.com>
2022-02-23 08:03:58 +0100

    go fmt applied

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-    "image"
+	"image"
 	"image/color"
 	_ "image/png"
 	"log"
-    "fmt"
 )
 
 const (
@@ -18,9 +18,8 @@ const (
 	tilesPerRow  = 6
 	tileWidth    = 32
 	tileHeight   = 32
-    palleteCols  = 6 
-    palleteRows  = 10 
-
+	palleteCols  = 6
+	palleteRows  = 10
 )
 
 type Game struct{}
@@ -40,70 +39,70 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func cursorInRect (cursor_x, cursor_y, rect_x1, rect_y1, rect_x2, rect_y2 int) bool {
-    if (cursor_x >= rect_x1 && cursor_x <= rect_x2) && (cursor_y >= rect_y1 && cursor_y <= rect_y2) {
-        return true
-    }
-    return false
+func cursorInRect(cursor_x, cursor_y, rect_x1, rect_y1, rect_x2, rect_y2 int) bool {
+	if (cursor_x >= rect_x1 && cursor_x <= rect_x2) && (cursor_y >= rect_y1 && cursor_y <= rect_y2) {
+		return true
+	}
+	return false
 }
 
 func drawCursorOnPallete(screen *ebiten.Image, x int, y int) {
-    ebitenutil.DrawRect(
-        screen,
-        float64((int(((x-palleteX)/tileWidth) * tileWidth) + palleteX)),
-        float64((int(((y-palleteY)/tileHeight) * tileHeight) + palleteY)),
-        tileWidth,
-        tileHeight,
-        color.White,
-    )
+	ebitenutil.DrawRect(
+		screen,
+		float64((int(((x-palleteX)/tileWidth)*tileWidth) + palleteX)),
+		float64((int(((y-palleteY)/tileHeight)*tileHeight) + palleteY)),
+		tileWidth,
+		tileHeight,
+		color.White,
+	)
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-    var currentTile int
+	var currentTile int
 
-    tilesetWidth, tilesetHeight := img.Size()
+	tilesetWidth, tilesetHeight := img.Size()
 
-	var tilesheetRows  int = tilesetWidth / tileWidth
-	var tilesheetCols int = tilesetHeight  / tileWidth
+	var tilesheetRows int = tilesetWidth / tileWidth
+	var tilesheetCols int = tilesetHeight / tileWidth
 	var tilesNum int = tilesheetCols * tilesheetRows
 
 	for currentTile < tilesNum {
 		tileRect := image.Rect(
-            (currentTile % tilesheetCols) * tileWidth,
-            (currentTile / tilesheetCols) * tileHeight,
-            ((currentTile % tilesheetCols) * tileWidth) + tileWidth,
-            ((currentTile / tilesheetCols) * tileWidth) + tileHeight,
-        ) 
+			(currentTile%tilesheetCols)*tileWidth,
+			(currentTile/tilesheetCols)*tileHeight,
+			((currentTile%tilesheetCols)*tileWidth)+tileWidth,
+			((currentTile/tilesheetCols)*tileWidth)+tileHeight,
+		)
 
 		subImg := img.SubImage(tileRect).(*ebiten.Image)
 		op := &ebiten.DrawImageOptions{}
-        op.GeoM.Translate(
-             float64(((currentTile % palleteCols) * tileWidth) + palleteX),
-             float64(((currentTile / palleteCols) * tileHeight) + palleteY),
-        )
+		op.GeoM.Translate(
+			float64(((currentTile%palleteCols)*tileWidth)+palleteX),
+			float64(((currentTile/palleteCols)*tileHeight)+palleteY),
+		)
 		screen.DrawImage(subImg, op)
 
 		currentTile++
-        if currentTile >= palleteCols * palleteRows {
-            break
-        }
+		if currentTile >= palleteCols*palleteRows {
+			break
+		}
 	}
 
-    // mouse
-    x,y := ebiten.CursorPosition()
-    msg := fmt.Sprintf("x=%d, y=%d \n", x, y)
+	// mouse
+	x, y := ebiten.CursorPosition()
+	msg := fmt.Sprintf("x=%d, y=%d \n", x, y)
 	ebitenutil.DebugPrint(screen, msg)
 
-    if cursorInRect(
-        x,
-        y,
-        palleteX,
-        palleteY,
-        (palleteX + (tileWidth * palleteCols)),
-        (palleteY + (tileHeight * palleteRows)),
-    ) {
-        drawCursorOnPallete(screen, x, y)
-    }
+	if cursorInRect(
+		x,
+		y,
+		palleteX,
+		palleteY,
+		(palleteX + (tileWidth * palleteCols)),
+		(palleteY + (tileHeight * palleteRows)),
+	) {
+		drawCursorOnPallete(screen, x, y)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
